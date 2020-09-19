5c5bdba82315df1270265a20353eed2e63b41926
zhs007 <sssxueren@gmail.com>
2020-09-19 20:24:57 +0800

    - added getBucketedTradesDay

--- bin/bitmex.js
+++ bin/bitmex.js
@@ -1,27 +1,47 @@
-const {getBucketedTrades} = require('../plugins/bitmex/utils');
+const {
+  getBucketedTrades,
+  getBucketedTradesDay,
+} = require('../plugins/bitmex/utils');
 const {string2timestamp} = require('../utils');
 
-getBucketedTrades({
-  binSize: '1m',
-  partial: false,
-  symbol: 'XBT',
-  count: 128,
-  start: 0,
-  reverse: false,
-  startTime: '2020-01-01T00:00:00.000Z',
-  endTime: '2020-01-02T00:00:00.000Z',
-})
-    .then((trades) => {
-    // console.log(trades);
-      for (let i = 0; i < trades.data.length; ++i) {
-        trades.data[i].ts = string2timestamp(trades.data[i].timestamp);
+// getBucketedTrades({
+//   binSize: '1m',
+//   partial: false,
+//   symbol: 'XBT',
+//   count: 128,
+//   start: 0,
+//   reverse: false,
+//   startTime: '2020-01-01T00:00:00.000Z',
+//   endTime: '2020-01-02T00:00:00.000Z',
+// })
+//     .then((trades) => {
+//     // console.log(trades);
+//       for (let i = 0; i < trades.data.length; ++i) {
+//         trades.data[i].ts = string2timestamp(trades.data[i].timestamp);
+//       }
+
+//       console.log(trades.data);
+
+//       // if (trades.data.length < 4096) {
+//       console.log('current length is ' + trades.data.length);
+//       // }
+
+//       process.exit();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       process.exit();
+//     });
+
+getBucketedTradesDay('XBTUSD', '20200101')
+    .then((candles) => {
+      for (let i = 0; i < trades.length; ++i) {
+        trades[i].ts = string2timestamp(trades[i].timestamp);
       }
 
-      console.log(trades.data);
+      console.log(trades);
 
-      // if (trades.data.length < 4096) {
-      console.log('current length is ' + trades.data.length);
-      // }
+      console.log('current length is ' + trades.length);
 
       process.exit();
     })
--- plugins/bitmex/utils.js
+++ plugins/bitmex/utils.js
@@ -1,4 +1,5 @@
 const {request} = require('../../request');
+const dayjs = require('dayjs');
 // const {awaitWrap} = require('../../utils');
 
 const API_URL = 'https://www.bitmex.com/api/v1';
@@ -6,7 +7,7 @@ const API_URL = 'https://www.bitmex.com/api/v1';
 /**
  * getBucketedTrades - Get previous trades in time buckets
  * @param {Object} params - {}
- * @return {Promise} Promise - then(trades) catch(err)
+ * @return {Promise} Promise - then(response) catch(err)
  */
 function getBucketedTrades(params) {
   return request(
@@ -16,4 +17,50 @@ function getBucketedTrades(params) {
   );
 }
 
+/**
+ * getBucketedTradesDay - Get previous trades in time buckets
+ * @param {string} symbol - symbol
+ * @param {string} day - day, it likes 20200101
+ * @return {Promise} Promise - then(trades) catch(err)
+ */
+function getBucketedTradesDay(symbol, day) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const stt = dayjs(day, 'YYYYMMDD').format('YYYY-MM-DDTHH:mm:ssZ[Z]');
+      const ent = dayjs(day, 'YYYYMMDD')
+          .add(1, 'day')
+          .format('YYYY-MM-DDTHH:mm:ssZ[Z]');
+
+      const candles = [];
+      while (true) {
+        const res = await getBucketedTrades({
+          binSize: '1m',
+          partial: false,
+          symbol: symbol,
+          count: 1000,
+          start: candles.length,
+          reverse: false,
+          startTime: stt,
+          endTime: ent,
+        });
+
+        if (res && res.data && Array.isArray(res.data)) {
+          for (let i = 0; i < res.data.length; ++i) {
+            candles.push(res.data[i]);
+          }
+        }
+
+        if (res.data.length == 0) {
+          break;
+        }
+      }
+
+      resolve(candles);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
 exports.getBucketedTrades = getBucketedTrades;
+exports.getBucketedTradesDay = getBucketedTradesDay;
