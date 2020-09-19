b89b735e5eaee7aefaa2a8b9271416db4d082240
zhs007 <sssxueren@gmail.com>
2020-09-19 19:25:51 +0800

    - Updated bitmex

--- bin/bitmex.js
+++ bin/bitmex.js
@@ -1,18 +1,28 @@
 const {getBucketedTrades} = require('../plugins/bitmex/utils');
+const {string2timestamp} = require('../utils');
 
 getBucketedTrades({
   binSize: '1m',
   partial: false,
   symbol: 'XBT',
-  count: 3,
+  count: 4096,
   start: 0,
   reverse: false,
   startTime: '2020-01-01T00:00:00.000Z',
   endTime: '2020-01-02T00:00:00.000Z',
 })
     .then((trades) => {
-      // console.log(trades);
+    // console.log(trades);
+      for (let i = 0; i < trades.data.length; ++i) {
+        trades.data[i].ts = string2timestamp(trades.data[i].timestamp);
+      }
+
       console.log(trades.data);
+
+      if (trades.data.length < 4096) {
+        console.log('current length is ' + trades.data.length);
+      }
+
       process.exit();
     })
     .catch((err) => {
